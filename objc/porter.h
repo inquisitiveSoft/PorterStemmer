f4e5fd434c0776509f7e5627fbb2d35d0ee8805e
Harry Jordan <harry@inquisitivesoftware.com>
2014-09-16 12:03:24 +0100

    Added attribution/header to .h file

--- objc/porter.h
+++ objc/porter.h
@@ -1,10 +1,13 @@
-//
-//  porter.h
-//
+/*
+	This is a header file for Martin Porter's stemming algorithm
+ 	coded up as thread-safe ANSI C by the author.
+	
+	See also http://www.tartarus.org/~martin/PorterStemmer
+*/
+
 
 #ifndef porter_h
 #define porter_h
-
 
 /* stemmer is a structure for a few local bits of data */
 
